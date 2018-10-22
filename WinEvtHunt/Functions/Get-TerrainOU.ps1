3bdc24573ba3851d2bde4d07c50cc46ebf6958fa
swearbear <43471727+swearbear@users.noreply.github.com>
2018-10-21 21:52:16 -0400

    Commented quite thoroughly

--- WinEvtHunt/Functions/Get-TerrainOU.ps1
+++ WinEvtHunt/Functions/Get-TerrainOU.ps1
@@ -5,17 +5,48 @@
         [string]$ZoneName
     )
     
-    $Site = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Sites |? Name -eq $SiteName
+    # Get a matching AD Site
+    $sitelist = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Sites
+    $site = $sitelist | Where-Object Name -eq $SiteName
 
     $OUs = @()
-    #$Site.Subnets
-    #Wait-Debugger
+
+    # This is a doozy of a one-liner, but considering how big certain DNS zones can be,
+    # I wanted to keep the evaluations chained so we aren't waiting around for every object
+    # to complete each step before begining the next.
     foreach ($net in $Site.Subnets) {
-        Get-DnsServerResourceRecord -ComputerName $Server -ZoneName $ZoneName -RRType A |
-            ? HostName -ne "@" | Test-InSubnet -Network $net.Name |Select -ExpandProperty HostName |
-                Get-ADComputer -ea SilentlyContinue |% {$_.DistinguishedName.Split(',', 2) |Select -Last 1} |
-                    Get-ADOrganizationalUnit |Select Name,DistinguishedName |
-                        % {if ($OUs -notcontains $_.DistinguishedName) {$OUs += $_.DistinguishedName; $_.DistinguishedName}}
+        Get-DnsServerResourceRecord -ComputerName $Server -ZoneName $ZoneName -RRType A | <#
+        Remove "@" hostnames because these are duplicates for our purposes.
+        #> Where-Object HostName -ne "@" | <#
+        Use custom function to test whether the A record's ip is in $net.
+        #> Test-InSubnet -Network $net.Name | <#
+        Drop ResourceRecord object and pass only the hostname as a string value.
+        #> Select-Object -ExpandProperty HostName | <#
+        Do an AD lookup for a computer object with hostname.  Ignore AD object not found errors.
+        #> Get-ADComputer -ErrorAction SilentlyContinue | <#
+        Do some stuff in a ForEach-Object function...
+        #> ForEach-Object
+            {
+                # Drop the AD object and pass along only the OU portion of the distinguished name as a string value.
+                $_.DistinguishedName.Split(',', 2) | <#
+                Use Select-Object to grab indexed positions when possible.  Null checks are built-in.
+                #> Select-Object -Last 1
+            } | <#
+        Get the AD object for the OU from the distinguished name we just grabbed.    
+        #> Get-ADOrganizationalUnit | <#
+        This select-object shouldn't require explaination.
+        #> Select-Object Name,DistinguishedName | <#
+        Do some more stuff in a ForEach-Object function...
+        #> ForEach-Object
+            {
+                # Add unique OUs to the OUs array
+                if ($OUs -notcontains $_.DistinguishedName)
+                {
+                    $OUs += $_.DistinguishedName
+                    # return OU dn to verbose stream for visual confirmation of progress
+                    Write-Verbose $_.DistinguishedName
+                }
+            }
     }
 
 }
--- WinEvtHunt/Functions/Test-InSubnet.ps1
+++ WinEvtHunt/Functions/Test-InSubnet.ps1
@@ -1,4 +1,16 @@
-﻿function Test-InSubnet {
+﻿<#
+.Synopsis
+   Test if an host IP address is part of a given subnet.
+.DESCRIPTION
+   Test if an host IP address is part of a given subnet.
+.EXAMPLE
+   Test-InSubnet -Address 10.54.4.11 -Network 10.54.0.0/16
+.EXAMPLE
+   $IPList | Test-InSubnet -Network 10.54.0.0/255.255.0.0 -PassThru
+
+   Instead of returning true/false, the true evaluations return the passed in object(s).
+#>
+function Test-InSubnet {
     param
     (
         [Parameter(Mandatory=$true,ValueFromPipeline=$true,ParameterSetName='Address')]
@@ -8,71 +20,81 @@
         [ciminstance[]]$RRInstance,
 
         [Parameter(Mandatory=$true)]
-        [string[]]$Network
+        [string[]]$Network,
+
+        [switch] $PassThru
     )
 
     begin
-    {   
+    {
         $IPv4Regex = '(?:(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)'
+        
+        function Convert-IPStringToUInt32 ([string] $ip) {
+            # Convert ipv4 address string to UInt32 array
+            $a = [uint32[]]$ip.Split('.')
+            # Reverse byte order, add values, assign to UInt32
+            [uint32] $uip = ($a[0] -shl 24) + ($a[1] -shl 16) + ($a[2] -shl 8) + $a[3]
+        }
 
-        #$network, [int]$subnetlen = $cidr.Split('/')
-
+        # Process all networks in the -Network parameter and apply their appropriate UInt32 values to an array
         $Subnets = @()
         foreach ($Subnet in $Network)
         {
             if (($Subnet -match "\A(?<IP>${IPv4Regex})\s*/\s*(?<NetworkLength>\d{1,2})\z") -or ("\A(?<IP>${IPv4Regex})[\s/]+(?<SubnetMask>${IPv4Regex})\z"))
             {
-                #Wait-Debugger
-                $a = [uint32[]]$Matches['IP'].Split('.')
-                [uint32] $unetwork = ($a[0] -shl 24) + ($a[1] -shl 16) + ($a[2] -shl 8) + $a[3]
+                $unetwork = Convert-IPStringToUInt32 -ip $Matches['IP']
+                
                 if ($Matches['SubnetMask'])
                 {
-                    $mask = [uint32[]]$Matches['SubnetMask'].Split('.')
+                    $umask = Convert-IPStringToUInt32 -ip $Matches['SubnetMask']
                 }
                 else
                 {
-                    $mask = (-bnot [uint32]0) -shl (32 - [int]$Matches['NetworkLength'])
+                    $umask = (-bnot [uint32]0) -shl (32 - [int]$Matches['NetworkLength'])
                 }
             }
             else
             {
-                Write-Error "Invalid subnet notation." -ErrorAction Stop
-                return
+                Write-Error -ErrorAction Stop -Message "Invalid subnet notation."
             }
 
-            $Subnets += @{uNetwork=$unetwork;Mask=$mask}
+            $Subnets += @{unetwork=$unetwork;mask=$umask}
         }
     }
 
     process
     {
-        try
+        # Parameter sets that match parameter names allow for dynamic calls like this
+        foreach ($obj in $PSBoundParameters[$PSCmdlet.ParameterSetName])
         {
             if ($PSCmdlet.ParameterSetName -eq 'RRInstance')
             {
-                $ip = $_.RecordData.IPv4Address.IPAddressToString
+                $ip = $obj.RecordData.IPv4Address.IPAddressToString
             }
             else
             {
-                $ip = $Address.IPAddressToString
+                $ip = $obj.IPAddressToString
             }
 
-            $a = [uint32[]]$ip.split('.')
-            [uint32] $uip = ($a[0] -shl 24) + ($a[1] -shl 16) + ($a[2] -shl 8) + $a[3]
+            $uip = Convert-IPStringToUInt32 -ip $ip
 
             foreach ($sn in $Subnets)
             {
-                if ($unetwork -eq ($mask -band $uip))
+                # compare ipv4 address to each of the networks in the -Network parameter
+                if ($sn.unetwork -eq ($sn.umask -band $uip))
                 {
-                    Write-Output $_
-                    break
+                    if ($PassThru)
+                    {
+                        Write-Output $_
+                    }
+                    else
+                    {
+                        Write-Output $true
+                    }
+
+                    break # Stop loop after the first match
                 }
             }
-        }
-        catch
-        {
-            $_
-            break
         }
     }
 }
